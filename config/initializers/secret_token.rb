2e3647fd50412dce6eebc5a6d0d0d117b312d6e4
cajorige <carlos.gregorio@waveweb2.com>
2011-06-02 16:42:27 +0100

    firts commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '6475d1c6567fdf31bbbf4f7b29c58d13599e188b6e4cf9c076e3a6392f682459738d4a3c9e7a4be8676f588f65b2f7de3055e3290a286e6f19b22059e798d0d5'
